d5150a266cb14b82db43ff74ead746cc1ade6ef7
zulnemekh <bekhbat_zulnemekh@unimedia.co.jp>
2018-07-27 18:39:00 +0800

    update

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,20 +1,109 @@
+const sha256 = require('sha256');
+const currentNodeUrl = process.argv[3];
+
 function Blockchain() {
 	this.chain = [];
-	this.newTransactions = [];
+	this.pendingTransaction = [];
+	this.networkNodes = [];
+	this.createNewBlock(100, '0', '0');
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
 	const newBlock = {
 		index: this.chain.length + 1,
 		timestamp: Date.now(),
-		transactions: this.newTransactions,
+		transactions: this.pendingTransaction,
 		nonce: nonce,
 		hash: hash,
 		previousBlockHash: previousBlockHash
 	};
 
-	this.newTransactions = [];
+	this.pendingTransaction = [];
 	this.chain.push(newBlock);
 
 	return newBlock;
 }
+
+Blockchain.prototype.getLastBlock = function() {
+	return this.chain[this.chain.length -1];
+}
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
+	const newTransaction = {
+		amount: amount,
+		sender: sender,
+		recipient: recipient
+	};
+
+	this.pendingTransaction.push(newTransaction);
+	
+	return this.getLastBlock()['index'] + 1;
+}
+
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
+	const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+	const hash = sha256(dataAsString);
+	return hash;
+}
+
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
+	// bitcoin.hashBlock = function(previousBlockHash, currentBlockData, nonce);
+	let nonce = 0;
+	let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+	while (hash.substring(0, 4) !== '0000'){
+		nonce++;
+		hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+	}
+
+	return nonce;
+}
+
+
+// register a node and broadcast it the network
+app.post('/register-and-broadcast-node', function(req, res) {
+	const newNodeUrl = req.body.newNodeUrl;
+	if(bitcoin.networkNodes.indexOf(newNodeUrl) == -1)
+		bitcoin.networkNodes.push(newNodeUrl);
+
+	const regNodesPromises = [];
+	bitcoin.networkNodes.forEach(networkNodeUrl => {
+		const requestOptions = {
+			uri: networkNodeUrl + '/register-node',
+			method: 'POST',
+			body: { newNodeUrl: newNodeUrl },
+			json: true
+		};
+
+		regNodesPromises.push(rp(requestOptions));
+	});
+
+	Promise.all(regNodesPromises).then(data => {
+		const bulkRegisterOptions = {
+			uri: newNodeUrl + '/register-nodes-bulk',
+			method: 'POST',
+			body: {allNetworkNodes: [ ...bitcoin.networkNodes, bitcoin.currentNodeUrl] },
+			json: true
+		};
+		return rp(bulkRegisterOptions);
+	}).then(data =>{
+		res.json({node: 'New node registered with network successfully.'});
+	});
+});
+
+// register a node with the network
+app.post('/register-node', function(req, res){
+	const newNodeUrl = req.body.newNodeUrl;
+	const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(newNodeUrl) == -1;
+	const notCurrentNode = bitcoin.currentNodeUrl !== newNodeUrl;
+
+	if (nodeNotAlreadyPresent && notCurrentNode) 
+	bitcoin.networkNodes.push(newNodeUrl);
+	res.json({node: 'New node registered with network successfully.'});
+});
+
+// register multiple nodes at once
+app.post('/register-node-bulk', function(req, res){
+
+});
+
+module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,5 +1,32 @@
-function Blockchain() {
-	this.chain = [];
-	this.newTransactions = [];
-	
-}
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+// bitcoin.createNewBlock(2389, 'OINA90SDNF90N', '90ANSD9F0N9009N');
+// bitcoin.createNewTransaction(100, 'ALEX90SDNF90N', 'JENN0SD9F0N909A9');
+
+const previousBlockHash = 'OINA90SDNF90NOINA90SDNF90N';
+const currentBlockData = [
+	{
+		amount: 10,
+		sender: '90ANSD9F0N9009N',
+		recipient: '10NA90SDNF90NR10N'
+	},
+	{
+		amount: 30,
+		sender: '300ANSD9F0N9009N',
+		recipient: 'OINA90SDNF90NR30N'
+	},
+	{
+		amount: 200,
+		sender: '200ANSD9F0N9009N',
+		recipient: 'OINA90SDNF90N200N'
+	},
+];
+
+
+// console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
+
+// console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 59112));
+// console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+console.log(bitcoin);
